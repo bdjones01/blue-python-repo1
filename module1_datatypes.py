21108c36e59029396dcaf13345a7c023850d1a19
Brittany Jones <britzay1009@gmail.com>
2023-06-04 05:33:38 +0000

    module1_datatypes

---
+++ module1_datatypes.py
@@ -0,0 +1,18 @@
+income = 250_000
+lowtaxland_rate = 0.05
+ripoffland_rate = 0.43
+
+print('Your income is', income, 'and you would pay', income * lowtaxland_rate, 'income tax in Lowtaxland or', income * ripoffland_rate, 'income tax in Ripoffland. You would save',  income * ripoffland_rate - income * lowtaxland_rate, 'by paying taxes in Lowtaxland!')
+
+age = 28
+print (age) 
+print ('What is your name?')
+user_name = input ()
+print ('Hello,', user_name)
+
+login = input ('Enter your login: ')
+language = input ('Enter your native language: ')
+
+print ('Your login is', login, 'and you speak', language)
+
+
